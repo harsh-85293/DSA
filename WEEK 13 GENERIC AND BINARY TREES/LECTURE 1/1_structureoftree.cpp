af18df2f19d352fd2db7e39702530cfefe925566
Harsh Ramchandani <harsh.ramchandani122003@gmail.com>
2024-11-12 09:43:56 +0530

    added new for tree

--- WEEK 13 GENERIC AND BINARY TREES/LECTURE 1/1_structureoftree.cpp
+++ WEEK 13 GENERIC AND BINARY TREES/LECTURE 1/1_structureoftree.cpp
@@ -35,10 +35,59 @@
             //1 case maine kar liya , baaki left and right recursion dekh lega
             cout << "adding left child for " << value  << endl;
             root -> left = createtree();
+
             cout << "adding right child for " << value  << endl;
             root -> right = createtree();
             return root;
         }
+    }
+
+    void preordertraversal(Node* root)
+    {
+        if(root == NULL)
+        {
+            return ;
+        }
+
+        //NLR
+        //N
+        cout << root->data << endl;
+        //L
+        preordertraversal(root->left);
+        //R
+        preordertraversal(root->right);
+    }
+
+    void inordertraversal(Node* root)
+    {
+        if(root == NULL)
+        {
+            return ;
+        }
+
+        //LNR
+        //L
+        inordertraversal(root->left);
+        //N
+        cout << root->data << endl;
+        //R
+        inordertraversal(root->right);
+    }
+
+    void postordertraversal(Node* root)
+    {
+        if(root == NULL)
+        {
+            return ;
+        }
+
+        //LRN
+        //L
+        postordertraversal(root->left);
+        //R
+        postordertraversal(root->right);
+        //N
+        cout << root->data << endl;
     }
 
 	int main()
