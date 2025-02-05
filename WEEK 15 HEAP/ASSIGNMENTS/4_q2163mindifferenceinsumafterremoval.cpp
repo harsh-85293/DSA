ac5b3ac8f74fcc4c60b53d851ada2369562ca83b
Harsh Ramchandani <harsh.ramchandani122003@gmail.com>
2025-02-05 23:38:35 +0530

    streak day2

--- WEEK 15 HEAP/ASSIGNMENTS/4_q2163mindifferenceinsumafterremoval.cpp
+++ WEEK 15 HEAP/ASSIGNMENTS/4_q2163mindifferenceinsumafterremoval.cpp
@@ -1,7 +1,45 @@
-   #include <bits/stdc++.h>
-   using namespace std;
+class Solution {
+public:
+    long long minimumDifference(vector<int>& nums) {
+        int n = nums.size() / 3;
+        int len = 3 * n;
+        
+        vector<long long> leftSum(len, 0), rightSum(len, 0);
+        priority_queue<int> maxHeap;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        long long sum = 0;
+        
+        // Compute leftSum: Minimum sum of first n elements from the left
+        for (int i = 0; i < len; i++) {
+            sum += nums[i];
+            maxHeap.push(nums[i]);
+            if (maxHeap.size() > n) {
+                sum -= maxHeap.top();
+                maxHeap.pop();
+            }
+            if (maxHeap.size() == n) leftSum[i] = sum;
+        }
 
-   int main()
-   {
-     return 0;
-   }
+        sum = 0;
+        
+        // Compute rightSum: Maximum sum of last n elements from the right
+        for (int i = len - 1; i >= 0; i--) {
+            sum += nums[i];
+            minHeap.push(nums[i]);
+            if (minHeap.size() > n) {
+                sum -= minHeap.top();
+                minHeap.pop();
+            }
+            if (minHeap.size() == n) rightSum[i] = sum;
+        }
+        
+        long long minDiff = LLONG_MAX;
+        
+        // Compute the minimum difference
+        for (int i = n - 1; i < 2 * n; i++) {
+            minDiff = min(minDiff, leftSum[i] - rightSum[i + 1]);
+        }
+        
+        return minDiff;
+    }
+};
